accedf6d4d94644465c26eead3ca39d0c38166a7
Max <max@pop-os.localdomain>
2022-06-13 13:34:49 -0700

    chnge application to app and delete debug=True on server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask_app import app as application
+from flask_app import app 
 
 from flask_app.controllers import users
 
 if __name__=="__main__":
-    application.run(debug=True)
+    # application.run(debug=True)
+    app.run()
